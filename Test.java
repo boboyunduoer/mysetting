0aa590449e8c780499c054fb47fb4d6c61cd940c
Bo Liu <liubodl@cn.ibm.com>
2019-01-17 13:15:19 +0800

    d

--- Test.java
+++ Test.java
@@ -33,7 +33,7 @@ public class Test {
         for (Field field: Test.class.getFields()) {
             System.out.println(field.getName());
         }
-         System.out.println("5");
+         System.out.println("6");
          System.out.println("1");
     }
 }
