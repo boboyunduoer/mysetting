15f3234f28488d6a1c7fa53631fbbeb3a5bff187
Bo Liu <liubodl@cn.ibm.com>
2019-01-17 12:44:48 +0800

    test

---
+++ Test.java
@@ -0,0 +1,37 @@
+
+import static java.util.Arrays.sort;
+
+import java.lang.reflect.Field;
+
+public class Test {
+
+     static String a = "dd";
+
+     private String phonenum;
+
+     public String getPhonenum() {
+         return this.phonenum;
+     }
+
+     public void setPhonenum(String phonenum) {
+     this.phonenum = phonenum;
+     }
+
+    static {
+
+        System.out.println("Class initilized");
+    }
+
+    public static void main (String[] args)
+    {
+        try {
+            System.out.println(Class.forName("Test").getDeclaredField("phonenum").getName());
+        } catch (NoSuchFieldException | SecurityException | ClassNotFoundException e1 ) {
+            e1.printStackTrace();
+        }
+        sort(new int[]{1,2,3,4,5});
+        for (Field field: Test.class.getFields()) {
+            System.out.println(field.getName());
+        }
+    }
+}
