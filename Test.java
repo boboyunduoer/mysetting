745873cb79d66c27d77af9cccc9ba9148ffb735b
boboyunduoer <44866551+boboyunduoer@users.noreply.github.com>
2019-01-17 14:10:11 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -19,7 +19,7 @@ public class Test {
 
     static {
         System.out.println("Class initilized");
-        System.out.println("rrrrr");
+        System.out.println("remoteremote");
     }
 
     public static void main (String[] args)
@@ -33,7 +33,7 @@ public class Test {
         for (Field field: Test.class.getFields()) {
             System.out.println(field.getName());
         }
-         System.out.println("rrrrr");
+         System.out.println("remoteremote");
          System.out.println("1");
     }
 }
