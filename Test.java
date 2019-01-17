06a67da11722fa84f39ab07865d4e5f07ca9dfb8
Bo Liu <liubodl@cn.ibm.com>
2019-01-17 13:47:19 +0800

    dev

--- Test.java
+++ Test.java
@@ -19,7 +19,7 @@ public class Test {
 
     static {
         System.out.println("Class initilized");
-        System.out.println("remote");
+        System.out.println("dev");
     }
 
     public static void main (String[] args)
@@ -33,7 +33,7 @@ public class Test {
         for (Field field: Test.class.getFields()) {
             System.out.println(field.getName());
         }
-         System.out.println("local");
+         System.out.println("dev");
          System.out.println("1");
     }
 }
