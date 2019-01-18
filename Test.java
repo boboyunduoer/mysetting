5f4ed249554c4d31c24878ef26f20f8928a70352
Bo Liu <liubodl@cn.ibm.com>
2019-01-18 20:47:43 +0800

    c

--- Test.java
+++ Test.java
@@ -19,7 +19,7 @@ public class Test {
 
     static {
         System.out.println("Class initilized");
-        System.out.println("remoteremote1");
+        System.out.println("remoteremote2");
     }
 
     public static void main (String[] args)
